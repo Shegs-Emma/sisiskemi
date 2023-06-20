d965edc5887d4910c27a9599117e076140bc7cc8
drew-chidi <chidi.andrew@gmail.com>
2023-06-20 12:15:59 +0100

    implemented bread crumbs

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,27 +1,53 @@
 import { Fragment } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+const pagePaths = ['Cart', 'Information', 'Shipping', 'Payment'];
+
 const Breadcrumbs = () => {
   const location = useLocation();
   const paths = location.pathname.split('/').filter((path) => path !== '');
 
   return (
-    <nav className='flex items-center space-x-2 text-sm text-gray-500'>
-      {paths.map((path, index) => (
-        <Fragment key={path}>
-          {index > 0 && <span className='text-gray-400'>/</span>}
-          <Link
-            to={`/${paths.slice(0, index + 1).join('/')}`}
-            className={`${
-              index === paths.length - 1 ? 'font-semibold' : 'text-red-500'
-            }`}
-          >
-            {path}
-          </Link>
-        </Fragment>
-      ))}
-    </nav>
+    <div className='mt-[30px]  px-5 mb-8 '>
+      <nav className='flex items-center space-x-2 text-sm text-gray-500'>
+        {pagePaths.map((path, index) => (
+          <Fragment key={path}>
+            {index > 0 && <span className='text-gray-400'>{'>'}</span>}
+            <Link
+              // key={path}
+              to={`/${paths.slice(0, index + 1).join('/')}`}
+              className={`${
+                index === paths.length - 1 ? 'font-semibold' : 'text-red-500'
+              }`}
+              // to={path}
+            >
+              {path}
+            </Link>
+          </Fragment>
+        ))}
+      </nav>
+    </div>
   );
 };
 
 export default Breadcrumbs;
+
+{
+  /* <div className='mt-[30px]  px-5 '>
+      <nav className='flex items-center space-x-2 text-sm text-gray-500'>
+        {paths.map((path, index) => (
+          <Fragment key={path}>
+            {index > 0 && <span className='text-gray-400'>{'>'}</span>}
+            <Link
+              to={`/${paths.slice(0, index + 1).join('/')}`}
+              className={`${
+                index === paths.length - 1 ? 'font-semibold' : 'text-red-500'
+              }`}
+            >
+              {path}
+            </Link>
+          </Fragment>
+        ))}
+      </nav>
+    </div> */
+}
--- src/components/CheckoutContent/index.jsx
+++ src/components/CheckoutContent/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import OrderSummary from './OrderSummary';
 import ContactForm from './ContactForm';
+import Breadcrumbs from '../Breadcrumbs';
 
 const CheckoutContent = () => {
   return (
     <div>
       <OrderSummary />
+      <Breadcrumbs />
       <ContactForm />
     </div>
   );
