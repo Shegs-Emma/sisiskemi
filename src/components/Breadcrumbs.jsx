2f759667c6c158113128a23490995412ec1c73cc
drew-chidi <chidi.andrew@gmail.com>
2023-06-20 18:49:10 +0100

    modify the bread crumbs

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -8,21 +8,23 @@ const Breadcrumbs = () => {
   const paths = location.pathname.split('/').filter((path) => path !== '');
 
   return (
-    <div className='mt-[30px]  px-5 mb-8 '>
+    <div className='mt-[30px] px-5 mb-8'>
       <nav className='flex items-center space-x-2 text-sm text-gray-500 md:justify-center'>
         {pagePaths.map((path, index) => (
           <Fragment key={path}>
-            {index > 0 && <span className='text-gray-400'>{'>'}</span>}
-            <Link
-              // key={path}
-              to={`/${paths.slice(0, index + 1).join('/')}`}
-              className={`${
-                index === paths.length - 1 ? 'font-semibold' : 'text-red-500'
-              }`}
-              // to={path}
-            >
-              {path}
-            </Link>
+            {index > 0 && <span>{'>'}</span>}
+            {index < paths.length ? (
+              <Link
+                to={`/${paths.slice(0, index + 1).join('/')}`}
+                className={`${
+                  index === paths.length - 1 ? 'font-semibold' : 'text-red-500'
+                }`}
+              >
+                {path}
+              </Link>
+            ) : (
+              <span>{path}</span>
+            )}
           </Fragment>
         ))}
       </nav>
@@ -31,23 +33,3 @@ const Breadcrumbs = () => {
 };
 
 export default Breadcrumbs;
-
-{
-  /* <div className='mt-[30px]  px-5 '>
-      <nav className='flex items-center space-x-2 text-sm text-gray-500'>
-        {paths.map((path, index) => (
-          <Fragment key={path}>
-            {index > 0 && <span className='text-gray-400'>{'>'}</span>}
-            <Link
-              to={`/${paths.slice(0, index + 1).join('/')}`}
-              className={`${
-                index === paths.length - 1 ? 'font-semibold' : 'text-red-500'
-              }`}
-            >
-              {path}
-            </Link>
-          </Fragment>
-        ))}
-      </nav>
-    </div> */
-}
